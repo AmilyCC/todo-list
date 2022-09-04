549d230e3abdf4eeaecbcfdd040c7df4eb3b7647
N000101\Amily Chang <aymaiclhyu777@gmail.com>
2022-09-04 21:26:33 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose') // 載入 mongoose
 const Todo = require('./models/todo') // 載入 Todo model
+// 引用 body-parser
+const bodyParser = require('body-parser')
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })// 設定連線到 mongoDB
 
@@ -18,7 +20,8 @@ db.once('open', () => {
 })
 app.engine('hbs',exphbs({defaultLayout:'main', extname:'.hbs'}))
 app.set('view engine','hbs')
-
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 // ...
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
@@ -26,6 +29,18 @@ app.get('/', (req, res) => {
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({name})
+  .then(() => res.redirect('/'))
+  .catch(error => console.log(error))
+})
+
 
 app.listen(3000,() =>{
   console.log('App is running on port http://localhost:3000')
