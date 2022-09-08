e66498eed433b9aa9ce84ce65efd4a8ab38674a5
N000101\Amily Chang <aymaiclhyu777@gmail.com>
2022-09-08 17:32:30 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
 const express = require('express');
-const app = express();
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose') // 載入 mongoose
-const Todo = require('./models/todo') // 載入 Todo model
 const methodOverride = require('method-override')
-// 引用 body-parser
 const bodyParser = require('body-parser')
+
 const routes = require('./routes')
+require('./config/mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })// 設定連線到 mongoDB
+const app = express();
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 app.engine('hbs',exphbs({defaultLayout:'main', extname:'.hbs'}))
 app.set('view engine','hbs')
-// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
 
 app.use(routes)
-// ...
-
 
 app.listen(3000,() =>{
   console.log('App is running on port http://localhost:3000')
